0de66c1432e1bb065a04a6ec8aa30a012c9d6c2f
slo0ey <rurchi1206@gmail.com>
2024-08-25 23:44:11 +0900

    import path 추가

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@src",
+        replacement: resolve(__dirname, "src"),
+      },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
+      {
+        find: "@assets",
+        replacement: resolve(__dirname, "src/assets"),
+      },
+      {
+        find: "@fonts",
+        replacement: resolve(__dirname, "src/assets/fonts"),
+      },
+      {
+        find: "@hooks",
+        replacement: resolve(__dirname, "src/hooks"),
+      },
+    ],
+  },
+  plugins: [react(), tsConfigPaths()],
+});
